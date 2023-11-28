109816376d4365c83c11589d236ff6676ee40d5f
Wacmoua <b.creteur@live.fr>
2023-11-28 23:22:16 +0100

    la c good

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let userInfo;
+
 document.getElementById("loginForm").addEventListener("submit", loginUser);
 
 async function getToken() {
@@ -53,8 +55,11 @@ async function loginUser(event) {
                 // Utilisez le token comme nécessaire
                 // ...
 
+                // Déclarez userInfo avec les informations de l'utilisateur
+                userInfo = data.user;
+
                 // Directement passer true à activateHomeTab si la connexion réussie
-                activateHomeTab(data.user, true);
+                activateHomeTab(userInfo, true);
 
                 // Appelez fetchData pour récupérer les données des posts après la connexion réussie
                 fetchData().then((data) => {
@@ -79,6 +84,7 @@ async function loginUser(event) {
     }
 }
 
+
 function activateHomeTab(userInfo, isLoggedIn) {
     const homeTab = document.getElementById("homeTab");
     homeTab.classList.add("active");
@@ -101,20 +107,26 @@ function activateHomeTab(userInfo, isLoggedIn) {
         loggedInUserInfo.innerHTML = `
             <h3>Bienvenue, ${userInfo.username} ! n'hesite pas a dire des truk!</h3>
             <!-- Vous pouvez ajouter d'autres informations de l'utilisateur ici -->
-            <div id="postContainer"></div>
+            
         `;
 
          // Créez et ajoutez le formulaire ici
          const newPostForm = document.createElement("form");
          newPostForm.classList.add("mb-3");
          newPostForm.innerHTML = `
-             <div class="form-group">
-                 <textarea class="form-control" rows="2" id="newPostMessage" placeholder="Écrire un nouveau post"></textarea>
-             </div>
-             <button type="button" class="btn btn-primary" id="postButton">Poster</button>
+         <div class="form-group col-md-8">
+         <textarea class="form-control" rows="2" id="newPostMessage" placeholder="Écrire un nouveau post"></textarea></br>
+         <button type="button" class="btn btn-dark" id="postButton">Poster</button>
+     </div>
+     
          `;
-         loggedInUserInfo.appendChild(newPostForm);
- 
+         
+         const postAll = document.createElement("div");
+        postAll.id="postContainer"
+    
+
+        loggedInUserInfo.appendChild(newPostForm);
+        loggedInUserInfo.appendChild(postAll);
          // Ajoutez l'écouteur d'événement
          const postButton = document.getElementById("postButton");
          if (postButton) {
@@ -140,6 +152,20 @@ document.addEventListener('DOMContentLoaded', () => {
         // Cachez le bouton de déconnexion
         document.getElementById("logoutTab").style.display = "none";
     }
+
+    const editPostForm = document.getElementById("editPostForm");
+    if (editPostForm) {
+        editPostForm.addEventListener("submit", async (event) => {
+            event.preventDefault();
+
+            // Récupérer l'ID du post en cours d'édition
+            const postId = document.getElementById("saveChangesButton").getAttribute("data-post-id");
+
+            // Appeler la fonction editPost avec l'ID du post
+            await editPost(postId);
+        });
+    }
+    
 });
 
 
@@ -210,6 +236,8 @@ async function fetchData() {
     }
 }
 
+
+
 async function addPost(event) {
     event.preventDefault();
 
@@ -219,8 +247,6 @@ async function addPost(event) {
     if (!(await checkToken())) {
         return;
     }
-
-    const token = localStorage.getItem('token');
 
     try {
         const token = await getToken();
@@ -249,6 +275,7 @@ async function addPost(event) {
     }
 }
 
+
 async function likePost(postId) {
     const token = await getToken();
     console.log("Token value:", token);
@@ -259,10 +286,6 @@ async function likePost(postId) {
     }
 
     try {
-        // Décoder le token JWT pour récupérer l'ID de l'utilisateur
-        const decodedToken = jwt_decode(token);
-        const userId = decodedToken.sub;
-
         const response = await fetch(
             `http://localhost:5000/post/like-post/${postId}`,
             {
@@ -271,116 +294,260 @@ async function likePost(postId) {
                     "Content-Type": "application/json",
                     "Authorization": `${token}`,
                 },
-                body: JSON.stringify({ userId }),
             }
         );
 
         const updatedPost = await response.json();
         console.log("Post liked:", updatedPost);
 
-        // Mettez à jour votre interface utilisateur en conséquence
-        displayPosts([updatedPost]); // Mettez à jour le post existant
+        // Récupérer les données mises à jour des posts après le like
+        const updatedPosts = await fetchData();
+
+        // Mettez à jour votre interface utilisateur en conséquence avec les nouveaux posts
+        displayPosts(updatedPosts);
     } catch (error) {
         console.error("Erreur lors du like du post:", error);
     }
+
+    
 }
 
-const newPostForm = document.createElement("form");
-newPostForm.classList.add("mb-3");
-newPostForm.innerHTML = `
-    <div class="form-group">
-        <textarea class="form-control" rows="2" id="newPostMessage" placeholder="Écrire un nouveau post"></textarea>
-    </div>
-    <button type="button" class="btn btn-primary" id="postButton">Poster</button>
-`;
-
-// Ajouter le formulaire au conteneur des posts une seule fois
-const postContainer = document.getElementById("postContainer");
-postContainer.appendChild(newPostForm);
-
-// Ajouter l'écouteur d'événement en dehors de la fonction
-const postButton = document.getElementById("postButton");
-if (postButton) {
-    postButton.addEventListener("click", addPost);
-}
-
-async function displayPosts(posts) {
-    const postContainer = document.getElementById("postContainer");
-
-    posts.forEach((post) => {
-        const postElement = document.createElement("div");
-        postElement.classList.add("col-md-8", "offset-md-2", "mb-3");
-
-        postElement.innerHTML = `
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-text">${post.message}</p>
-                    <p class="card-text">
-                        <small class="text-muted">${post.author}</small></br>
-                        <small class="text-muted">${post.createdAt}</small></br>
-                        <small class="text-muted-likers likers">Liked by: ${post.likers.map(liker => liker.username).join(', ')}</small>
-                    </p>
-                    <button class="btn btn-primary like-button" data-post-id="${post._id}">Like</button>
-                </div>
-                
-                <!-- Ajout du formulaire de commentaire -->
-                <form class="card-footer">
-                    <div class="form-group">
-                        <textarea class="form-control" rows="1" placeholder="Commenter"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Comment'</button>
-                </form>
-            </div>
-        `;
-
-        postContainer.appendChild(postElement);
-
-        const likeButton = postElement.querySelector(".like-button");
-        if (likeButton) {
-            likeButton.addEventListener("click", async () => {
-                const postId = likeButton.getAttribute("data-post-id");
-                await likePost(postId);
-            });
-        }
-
-        // Afficher les noms d'utilisateur des likers
-        const likersElement = postElement.querySelector(".text-muted-likers");
-        console.log("Likers data:", post.likers);
-        likersElement.innerHTML = post.likers.map(liker => liker.username).join(', ');
-    });
-}
-
-async function getLikersUsernames(likers) {
+async function dislikePost(postId) {
     const token = await getToken();
     console.log("Token value:", token);
 
     if (!token) {
-        console.error("Token d'utilisateur manquant. L'utilisateur doit être connecté pour récupérer les likers.");
-        return '';
+        console.error("Token d'utilisateur manquant. L'utilisateur doit être connecté pour ne pas aimer un post.");
+        return;
     }
 
     try {
-        const response = await fetch("http://localhost:5000/user/usernames", {
-            method: "POST",
+        const response = await fetch(
+            `http://localhost:5000/post/dislike-post/${postId}`,
+            {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `${token}`,
+                },
+            }
+        );
+
+        const updatedPost = await response.json();
+        console.log("Post disliked:", updatedPost);
+
+        const updatedPosts = await fetchData();
+
+        // Mettez à jour votre interface utilisateur en conséquence
+        displayPosts(updatedPosts); // Mettez à jour le post existant
+    } catch (error) {
+        console.error("Erreur lors du dislike du post:", error);
+    }
+}
+
+async function editPost(postId) {
+    const token = await getToken();
+    console.log("Token value:", token);
+
+    if (!token) {
+        console.error("Token d'utilisateur manquant. L'utilisateur doit être connecté pour éditer un post.");
+        return;
+    }
+
+    try {
+        // Récupérer le contenu actuel du post
+        const currentContent = document.getElementById("editedPostContent").value;
+
+        const response = await fetch(`http://localhost:5000/post/${postId}`, {
+            method: "PUT",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `${token}`,
             },
             body: JSON.stringify({
-                userIds: likers,
+                message: currentContent,
             }),
         });
 
-        const data = await response.json();
+        const updatedPost = await response.json();
+        console.log("Post édité:", updatedPost);
 
-        if (!response.ok) {
-            console.error("Erreur lors de la récupération des noms d'utilisateur des likers:", data.message);
-            return '';
-        }
+        // Fermer la fenêtre modale après l'édition
+        $('#editPostModal').modal('hide');
 
-        return data.usernames.join(', ');
+        // Récupérer les données mises à jour des posts après l'édition
+        const updatedPosts = await fetchData();
+
+        // Mettez à jour votre interface utilisateur en conséquence avec les nouveaux posts
+        displayPosts(updatedPosts);
     } catch (error) {
-        console.error("Erreur lors de la récupération des noms d'utilisateur des likers:", error);
-        return '';
+        console.error("Erreur lors de l'édition du post:", error);
     }
 }
+
+async function deletePost(postId) {
+    try {
+      const token = await getToken();
+      console.log("Token d'authentification:", token);
+
+      const response = await fetch(`http://localhost:5000/post/${postId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `${token}`,
+        },
+      });
+  
+      if (response.ok) {
+        console.log("Post deleted successfully");
+        // Mettez à jour votre interface utilisateur en conséquence
+        // ...
+      } else {
+        const data = await response.json();
+        console.error("Error deleting post:", data.message);
+      }
+    } catch (error) {
+      console.error("Error deleting post:", error);
+    }
+  }
+  
+
+
+async function displayPosts(posts) {
+    const postContainer = document.getElementById("postContainer");
+    postContainer.innerHTML = "";
+
+    // Ajoutez une vérification pour s'assurer que posts est défini avant de l'itérer
+    if (posts && Array.isArray(posts)) {
+        posts.forEach((post) => {
+            const postElement = document.createElement("div");
+            postElement.classList.add("col-md-8", "offset-md-2", "mb-3");
+
+            postElement.innerHTML = `
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">${post.message}</p>
+                        <p class="card-text">
+                            <small class="text-muted">${post.author.username}</small></br>
+                            <small class="text-muted">${post.createdAt}</small></br>
+                            <small class="text-primary likers">Liked by: ${post.likers.map(liker => liker.userId.username).join(', ')}</small></br>
+                            <small class="text-danger dislikers">Disliked by: ${post.dislikers.map(disliker => disliker.userId.username).join(', ')}</small>
+                           
+
+                        </p>
+                        <button class="btn btn-primary like-button" data-post-id="${post._id}">Like</button>
+                        <button class="btn btn-danger dislike-button" data-post-id="${post._id}">Dislike</button>
+                        
+                    </div>
+                    
+                    <!-- Ajout du formulaire de commentaire -->
+                    <form class="card-footer">
+                        <div class="form-group">
+                            <textarea class="form-control" rows="1" placeholder="Commenter"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success">Comment'</button>
+                    </form>
+                </div>
+            `;
+
+            postContainer.appendChild(postElement);
+
+            const likeButton = postElement.querySelector(".like-button");
+            if (likeButton) {
+                likeButton.addEventListener("click", async () => {
+                    const postId = likeButton.getAttribute("data-post-id");
+                    console.log("like button clicked for post ID:", postId);
+                    await likePost(postId);
+                });
+            }
+
+            const dislikeButton = postElement.querySelector(".dislike-button");
+            if (dislikeButton) {
+                dislikeButton.addEventListener("click", async () => {
+                    const postId = dislikeButton.getAttribute("data-post-id");
+                    console.log("Dislike button clicked for post ID:", postId);
+                    await dislikePost(postId);
+                });
+            }
+
+            
+
+         
+
+            if (post.author._id === userInfo._id) {
+            const editButton = document.createElement("button");
+            editButton.classList.add("btn", "btn-warning", "edit-button");
+            editButton.setAttribute("data-toggle", "modal");
+            editButton.setAttribute("data-target", "#editPostModal");
+            editButton.setAttribute("data-post-id", post._id); // Ajoutez cet attribut pour stocker l'ID du post
+            editButton.innerText = "Éditer";
+            
+            // Ajoutez l'écouteur d'événements pour le bouton "Éditer"
+            editButton.addEventListener("click", () => {
+                const postId = editButton.getAttribute("data-post-id");
+                console.log("Éditer le bouton cliqué pour le post ID:", postId);
+            
+                // Récupérer le contenu actuel du post
+                const currentContent = post.message;
+            
+                // Remplir le champ de saisie du modal avec le contenu actuel
+                const editedPostContent = document.getElementById('editedPostContent');
+                editedPostContent.value = currentContent;
+            
+                // Stocker l'ID du post en cours d'édition dans un attribut du bouton "Enregistrer les modifications"
+                const saveChangesButton = document.getElementById("saveChangesButton");
+                if (saveChangesButton) {
+                    saveChangesButton.setAttribute("data-post-id", postId);
+                }
+            
+                // Ouvrir le modal d'édition
+                $('#editPostModal').modal('show');
+            });
+
+            postElement.appendChild(editButton);
+        }
+
+  
+    if (post.author._id === userInfo._id) {
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn", "btn-danger", "delete-button");
+    deleteButton.setAttribute("data-post-id", post._id);
+    deleteButton.innerText = "Supprimer";
+  
+    // Ajouter l'écouteur d'événements pour le bouton de suppression
+    deleteButton.addEventListener("click", async () => {
+      try {
+        const postId = deleteButton.getAttribute("data-post-id");
+        console.log("Supprimer le bouton cliqué pour le post ID:", postId);
+  
+        // Appeler la fonction de suppression côté client
+        await deletePost(postId);
+  
+        // Mettez à jour votre interface utilisateur en conséquence
+        // ...
+  
+        // Vous pouvez également supprimer l'élément du post du DOM si nécessaire
+        postElement.remove();
+      } catch (error) {
+        console.error("Erreur lors de la suppression du post:", error);
+      }
+    });
+  
+    // Ajouter le bouton de suppression à l'élément du post
+    postElement.appendChild(deleteButton);
+  }
+  // ...
+  
+// ...
+
+         });
+        }
+      }
+
+        
+
+
+
+
+
+
